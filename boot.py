82f8896a59e0d9272f79d149fd3c74faa92c57a0
Johannes Graeter <johannes.graeter@kit.edu>
2018-01-08 19:47:59 +0100

    put it on feather

--- boot.py
+++ boot.py
@@ -2,12 +2,15 @@
 #import esp
 #esp.osdebug(None)
 import gc
-import webrepl
+#import webrepl
 from connect_to_router import *
 
 # webrepl stuff
-webrepl.start()
+#webrepl.start()
 gc.collect()
 
 # connect to router, config has fields ssid and password, optional: static_ip
 do_connect("./config_wifi.json")
+
+gc.collect()
+gc.mem_free()
